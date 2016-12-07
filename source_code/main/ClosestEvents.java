c9edfed98bd8189d0d039bd302908c5f44826f58
Khalil Ahmed <mkhalila@outlook.com>
2016-12-07 01:38:10 +0000

    Declares Closest Events main class

---
+++ source_code/main/ClosestEvents.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Class that represents the retrieval of Closest Events to a given location given by the user via a Scanner.
+ * Main is stored in this class.
+ */
+public class ClosestEvents {
+    public static void main(String[] args) {
+
+    }
+}
