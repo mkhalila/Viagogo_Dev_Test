a8278b634c8e47532d155fe0be115f7db4e4d2ca
Khalil Ahmed <mkhalila@outlook.com>
2016-12-07 02:03:26 +0000

    Retrieves user location via user input

--- source_code/main/ClosestEvents.java
+++ source_code/main/ClosestEvents.java
@@ -1,5 +1,9 @@
 package main;
 
+import javafx.util.Pair;
+
+import java.util.Scanner;
+
 /**
  * Class that represents the retrieval of Closest Events to a given location given by the user via a Scanner.
  * Main is stored in this class.
@@ -7,5 +11,13 @@ package main;
 public class ClosestEvents {
     public static void main(String[] args) {
         World world = new World(new Range(-10, 10), new Range(-10, 10));
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your location (e.g. \"4,2\"):");
+        String userInput = scanner.nextLine();
+        Pair<Integer, Integer> userLoc = new Pair<>(Integer.parseInt(String.valueOf(userInput.charAt(0))),
+                Integer.parseInt(String.valueOf(userInput.charAt(2))));
+
     }
 }
